47d745f372cfde01f7722e628daa47b033ebd98e
admin <admin@new-host-4.home>
2013-02-01 22:45:12 -0500

    fixxer

--- ad.py
+++ ad.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import itertools
 import logging
 import os
 import random
@@ -13,45 +14,56 @@ skype = Skype4Py.Skype()
 # Connect the Skype object to the Skype client.
 skype.Attach()
 
+#     letsjoin =  '#lkcglobal/$2d2b21b71349065e',
+
 rooms = dict(
     unlimited_ad = '#debbiematics/$b3691abf8f26222',
-    letsjoin =  '#lkcglobal/$2d2b21b71349065e'
+    perfect_trade = '#lordking989/$ac4d25d36c595eea',
+    new_programs = '#jimfurr/$2863804c49a01247'
 )
 
-def post_ad(text):
-    logging.debug("Posting {0}".format(text))
-    c = skype.Chat(rooms['unlimited_ad'])
-    c.SendMessage(text)
-
-import os
+room_cycle = itertools.cycle(sorted(rooms.keys()))
 
 ad_path = "ads"
-ad_delay = 60 # minutes
+ad_delay = 45 # minutes
+
+
+def post_ad(file):
+    with open(file, 'r') as f:
+        ad_copy = f.read()
+        c = skype.Chat(rooms[ random.choice(rooms.keys()) ])
+        c.SendMessage(ad_copy)
 
 def minutes_to_seconds(m):
     return m * 60
 
 def wanted_ad(filename):
-    desired = 'xchanger jubi traffic uinvest potis elias silver-saver solavei eurex ipdn'.split()
-    return any (
-        filename.startswith(s) and not filename.endswith('~')
-        for s in desired
-    )
+    desired = 'asn:tax elias eurex gold ipdn jubi potis silver-saver solavei traffic uinvest xchanger'.split()
+    return any (s in filename for s in desired)
+
+def random_file():
+    files = [os.path.join(path, filename)
+             for path, dirs, files in os.walk(ad_path)
+             for filename in files
+             if not filename.endswith("~")]
+    return random.choice(files)
+
+def get_wanted_file():
+    file = random_file()
+    if not wanted_ad(file):
+        print "\tskipping {0}".format(file)
+        return get_wanted_file()
+    else:
+        print "\tposting {0}".format(file)
+        return file
+
+
 
 def post_ads():
-    for (path, dirs, files) in os.walk(ad_path):
-        for file in files:
-            print file
-            if not wanted_ad(file):
-                print "\tskipping"
-                continue
-            else:
-                print "\tposting"
-            fullpath = os.path.join(path, file)
-            with open(fullpath, 'r') as f:
-                ad_copy = f.read()
-                post_ad(ad_copy)
-                time.sleep( minutes_to_seconds(60) )
+    while True:
+        file = get_wanted_file()
+        post_ad(file)
+        time.sleep( minutes_to_seconds(ad_delay) )
 
 def main():
     while True:
